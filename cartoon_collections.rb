b57b924af92a8a66a81228033307ba6313f67837
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 16:56:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,9 @@ def summon_captain_planet(calls)
   
 end
 
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
 
 # def find_the_cheese# code an argument here
 #   # the array below is here to help

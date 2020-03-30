8af191e744ca773adbc68fc2c2bcfdb34a62f7ad
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 17:08:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,8 @@ def long_planeteer_calls(words)
   tf_array.include?("true")
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(snacks)
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    
+    snacks.include?()
 end

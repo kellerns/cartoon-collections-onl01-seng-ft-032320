f2c78912e2f1c2cd882504970bb0500bd5e1f51c
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 16:47:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  
+  
+  
 end
 
 # def long_planeteer_calls# code an argument here
